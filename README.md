f3207500888a44e6a82adf938d4eb7118812614d
Gagaro <gagaro42@gmail.com>
2016-03-29 15:59:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# tinymce-editattributes
+A plugin allowing edition and addition of attributes for TinyMCE 4
+
+How to install the plugin
+=========================
+
+1. copy `editattributes` folder to the tinymce `plugins` folder.
+
+2. set up your instance in the tinyMCE.init method to use the plugin, like::
+
+  plugins : "... editattributes ..."
+
+3. set up your instance to use the imgmap button, for example::
+
+  toolbar : "... | editattributes"
---
+++ editattributes/plugin.min.js
@@ -0,0 +1,64 @@
+(function(){
+
+tinymce.PluginManager.add('editattributes', function(editor) {
+	function showDialog() {
+		var selectedNode = editor.selection.getNode();
+
+		var body = [];
+
+		for (var i = 0 ; i < selectedNode.attributes.length ; i++) {
+			var attribute = selectedNode.attributes[i];
+
+			body.push({type: 'textbox', name: attribute.name, size: 40, label: attribute.name, value: attribute.value});
+		}
+		body.push({
+			type: 'container',
+			label: 'New attribute',
+			layout: 'flex',
+			direction: 'row',
+			align: 'center',
+			spacing: 5,
+			items: [
+				{name: 'mce_new_name', type: 'textbox', ariaLabel: 'Name'},
+				{name: 'mce_new_value', type: 'textbox', ariaLabel: 'Value'},
+			]
+		});
+
+		editor.windowManager.open({
+			title: 'Edit Attributes',
+			body: body,
+			onsubmit: function(e) {
+				editor.undoManager.transact(function() {
+					var new_name = e.data['mce_new_name'];
+					var new_value = e.data['mce_new_value'];
+
+					delete e.data['mce_new_name'];
+					delete e.data['mce_new_value'];
+
+					if (new_name.length > 0) {
+						e.data[new_name] = new_value;
+					}
+					for (key in e.data) {
+						editor.dom.setAttribs(selectedNode, e.data);
+					}
+				});
+			}
+		});
+	}
+
+	//editor.addCommand('mceEditAttributes', showDialog);
+
+	editor.addButton('editattributes', {
+		icon: 'anchor',
+		tooltip: 'Edit Attributes',
+		onclick: showDialog
+	});
+
+	editor.addMenuItem('editattributes', {
+		icon: 'editattributes',
+		text: 'Edit Attributes',
+		onclick: showDialog
+	});
+});
+
+}());
